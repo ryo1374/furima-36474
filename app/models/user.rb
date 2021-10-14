a8bdf6d0a362fa265fe17fdef32dadddf1885f10
ryo1374 <rxyxomu419@gmail.com>
2021-10-14 15:35:43 +0900

    新規登録/本人情報確認

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: ". Include both letters and numbers" }
+
   with_options presence: true do
     validates :nickname
-    validates :last_name, format: { with: /\A[一-龥ぁ-ん]/, message: "is invalid. Input full-width characters" }
-    validates :first_name
-    validates :last_kana
-    validates :first_kana
+    validates :last_name,  format: { with: /\A[一-龥ぁ-ん]/, message: "is invalid. Input full-width characters" }
+    validates :first_name, format: { with: /\A[一-龥ぁ-ん]/, message: "is invalid. Input full-width characters" }
+    validates :last_kana,  format: { with: /\A[ァ-ヴー]+\z/u, message: "is invalid. Input full-width katakana characters" }
+    validates :first_kana, format: { with: /\A[ァ-ヴー]+\z/u, message: "is invalid. Input full-width katakana characters" }
     validates :birthday
   end
 end
