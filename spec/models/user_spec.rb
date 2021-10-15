5f6987a85c2ba4340b9f9018f275bc40b2c1e24b
ryo1374 <rxyxomu419@gmail.com>
2021-10-15 15:44:57 +0900

    テストコード修正2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -101,6 +101,13 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
 
+      it 'passwordに全角が含まれていると登録できない' do
+        @user.password = '123abあ'
+        @user.password_confirmation = '123abあ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password . Include both letters and numbers')
+      end
+
       it 'passwordとpassword_confirmationが不一致では登録できない' do
         @user.password = '123abc'
         @user.password_confirmation = '123abd'
