e806066bf224eadde940f31ebf950307fcd70801
ryo1374 <rxyxomu419@gmail.com>
2021-10-17 12:16:29 +0900

    バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,10 +9,14 @@ class Item < ApplicationRecord
   belongs_to_active_hash :ship
   
   with_options presence: true do
+    validates :image
     validates :item_name
     validates :item_description
     validates :price
   end
+
+    validates :price, numericality: { with: /\A[0-9]+\z/,message: 'is invalid. Input half-width characters' }
+    validates :price, numericality: { greater_than: 300, less_than: 9999999, message: 'is out of setting range' }
 
   validates :category_id, :status_id, :fee_id, :area_id, :ship_id,  numericality: { other_than: 1, message: "can't be blank" } 
 end
