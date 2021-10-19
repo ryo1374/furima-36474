4cfbb781e77e4e4501fa9a0e1150602faf0d4119
ryo1374 <rxyxomu419@gmail.com>
2021-10-19 11:30:51 +0900

    販売手数料・利益の表示

---
+++ app/javascript/price.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', function(){
+// 金額を入力した数値をpriceInputという変数に格納する
+const priceInput = document.getElementById("item-price");
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  console.log(inputValue);
+
+  const addTaxDom = document.getElementById("add-tax-price");
+  addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
+  console.log(addTaxDom);
+
+  const profitAmount = document.getElementById("profit");
+  const tax = inputValue * 0.1
+  profitAmount.innerHTML = (Math.floor(inputValue - tax));
+  console.log(profitAmount);
+  });
+});
