ecb254bd6539caf8a36d3ca2c67e87a77be82e22
ryo1374 <rxyxomu419@gmail.com>
2021-11-06 12:43:37 +0900

    実装完了

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,42 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+
   def index
     @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
+    if @item.order.present?
+      redirect_to root_path
+    end
   end
 
   def create
     @order_address = OrderAddress.new(order_params)
     @item = Item.find(params[:item_id])
     if @order_address.valid?
+      pay_item
       @order_address.save
-      redirect_to action: :index
+      redirect_to root_path
     else
       render action: :index
     end
   end
 
+  def show
+    @item = Item.find(params[:item_id])
+  end
+
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :area_id, :municipalities, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:postal_code, :area_id, :municipalities, :address, :building_name, :phone_number, :price).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,  
+      card: order_params[:token],   
+      currency: 'jpy'                 
+    )
   end
 end
--- app/models/OrderAddress.rb
+++ app/models/OrderAddress.rb
@@ -1,8 +1,9 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :area_id, :municipalities, :address, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :area_id, :municipalities, :address, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
+    validates :token
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :municipalities
@@ -12,6 +13,6 @@ class OrderAddress
     validates :phone_number, length: { is: 11, message: "is too short"}
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, area_id: area_id, municipalities: municipalities, address: address, building_name: building_name, phone_number: phone_number, user_id: user.id, order_id: order.id) 
+    Address.create(postal_code: postal_code, area_id: area_id, municipalities: municipalities, address: address, building_name: building_name, phone_number: phone_number) 
   end
 end
